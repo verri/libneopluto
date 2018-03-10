0e6627017fe17e4d98990eb636053ca9e53d6226
Filipe Verri <filipeverri@gmail.com>
2018-03-10 15:15:37 -0300

    Date, value and tag of the entry.

--- src/entry.cpp
+++ src/entry.cpp
@@ -91,23 +91,94 @@ auto entry::retrieve_description() const -> std::string
   return name;
 }
 
+auto entry::update_date(const date d) -> void
+{
+  assert(db);
+
+  std::stringstream ss;
+  ss << "UPDATE Entry SET date = " << d.to_integer() << " WHERE id = " << id << ';';
+
+  db->exec_query(ss.str().c_str());
+}
+
+auto entry::retrieve_date() const -> date
+{
+  assert(db);
+
+  std::stringstream ss;
+  ss << "SELECT date FROM Entry WHERE id = " << id << ";";
+
+  date d;
+  db->exec_query(ss.str().c_str(), [&d](sqlite3_stmt* stmt) {
+    d = date::from_integer(sqlite3_column_int(stmt, 0));
+    return false;
+  });
+
+  return d;
+}
+
+auto entry::update_value(std::int64_t value) -> void
+{
+  assert(db);
+
+  std::stringstream ss;
+  ss << "UPDATE Entry SET value = " << value << " WHERE id = " << id << ';';
+
+  db->exec_query(ss.str().c_str());
+}
+
+auto entry::retrieve_value() const -> std::int64_t
+{
+  assert(db);
+
+  std::stringstream ss;
+  ss << "SELECT value FROM Entry WHERE id = " << id << ";";
+
+  std::int64_t value;
+  db->exec_query(ss.str().c_str(), [&value](sqlite3_stmt* stmt) {
+    value = sqlite3_column_int(stmt, 0);
+    return false;
+  });
+
+  return value;
+}
+
 auto entry::update_tag(const std::optional<tag>& t) -> void
 {
   assert(db);
-  if (!t) {
-    std::stringstream ss;
-    ss << "UPDATE Entry SET tag = NULL WHERE id = " << id << ';';
-    db->exec_query(ss.str().c_str());
-    return;
-  }
-
-  assert(t->id > 0);
-  assert(t->db == db);
+  assert(!db || t->id > 0);
+  assert(!db || t->db == db);
 
   std::stringstream ss;
-  ss << "UPDATE Entry SET tag = " << t->id << " WHERE id = " << id << ';';
+  ss << "UPDATE Entry SET tag = ";
+
+  if (t)
+    ss << t->id;
+  else
+    ss << "NULL";
+
+  ss << " WHERE id = " << id << ';';
 
   db->exec_query(ss.str().c_str());
+}
+
+auto entry::retrieve_tag() const -> std::optional<tag>
+{
+  assert(db);
+
+  std::stringstream ss;
+  ss << "SELECT tag FROM Entry WHERE id = " << id << ";";
+
+  std::int64_t id = -1;
+  db->exec_query(ss.str().c_str(), [&](sqlite3_stmt* stmt) {
+    id = sqlite3_column_int(stmt, 0);
+    return false;
+  });
+
+  if (id == -1)
+    return std::nullopt;
+
+  return tag{id, db};
 }
 
 auto entry::erase() -> void
